4ea185f196d88de30e9e546f422538e2b278f727
alex troshkin <alextroshkin@outlook.com>
2017-12-27 02:00:28 +0300

    Added warppers for  - query async  - query single or default async  - execture async

---
+++ src/Database.fs
@@ -0,0 +1,48 @@
+﻿namespace FSharp.Data.Dapper
+
+open Dapper
+open System.Data
+open Microsoft.FSharp.Core.Operators.Unchecked
+
+    type QueryDefinition = 
+        { Script : string
+          Parameters : obj option }
+
+[<AutoOpen>]
+module Database =
+    
+    let private await task = task |> Async.AwaitTask
+
+    let private parametersOf qDef =
+        match qDef.Parameters with
+        | Some p -> p
+        | None -> null
+
+
+    let queryAsync<'TRow> 
+        (queryDefinition : QueryDefinition) 
+        (connection : IDbConnection) = async {
+
+        return await <| connection.QueryAsync<'TRow>(queryDefinition.Script, parametersOf queryDefinition)
+    }
+
+    let querySingleOrDefault<'T when 'T : equality >
+        (queryDefinition : QueryDefinition)
+        (connection : IDbConnection) = async {
+
+        let! single = await <| connection.QuerySingleOrDefaultAsync<'T>(queryDefinition.Script, parametersOf queryDefinition)
+
+        return 
+            match single with
+            | value when value = defaultof<'T> -> None
+            | value -> Some value
+    }
+
+    let executeAsync 
+        (queryDefinition : QueryDefinition)
+        (connection : IDbConnection) = async {
+
+        let countOfAffectedRows = await <| connection.ExecuteAsync(queryDefinition.Script, parametersOf queryDefinition)
+
+        return countOfAffectedRows
+    }
