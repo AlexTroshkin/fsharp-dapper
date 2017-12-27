dc25c2822baf1621ee038f68aebeab666d339537
alex troshkin <alextroshkin@outlook.com>
2017-12-27 03:36:04 +0300

    Added test project

---
+++ tests/FSharp.Data.Dapper.Tests/Program.fs
@@ -0,0 +1,12 @@
+﻿open Expecto
+
+[<Tests>]
+let tests =
+  test "A simple test" {
+    let subject = "Hello World"
+    Expect.equal subject "Hello World" "The strings should equal"
+  }
+
+[<EntryPoint>]
+let main argv =
+    runTestsInAssembly defaultConfig argv
