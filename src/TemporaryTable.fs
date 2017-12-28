8795a465066245d1c793075d12e66b8ad2dc727f
Alex Troshkin <alextroshkin@outlook.com>
2017-12-28 17:54:04 +0300

    Added temporary table module and definition

---
+++ src/TemporaryTable.fs
@@ -0,0 +1,12 @@
+﻿namespace FSharp.Data.Dapper
+
+open System.Collections
+
+[<AutoOpen>]
+module TemporaryTable =
+
+    type TemporaryTableDefinition = 
+        { Name : string 
+          Rows : IEnumerable }
+
+        
