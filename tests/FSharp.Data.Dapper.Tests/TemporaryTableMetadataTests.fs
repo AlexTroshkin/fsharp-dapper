c7447fa4a041e23dbd146f4f57e4501fdf61df12
alex troshkin <alextroshkin@outlook.com>
2018-01-02 20:12:03 +0300

    Added test for generating temporary table metadata

---
+++ tests/FSharp.Data.Dapper.Tests/TemporaryTableMetadataTests.fs
@@ -0,0 +1,28 @@
+﻿module TemporaryTableMetadataTests
+
+open Expecto
+
+open InMemoryDatabase.Types
+
+open FSharp.Data.Dapper
+open FSharp.Data.Dapper.TemporaryTable
+
+[<Tests>]
+let tests = 
+    testList "Temporary table metadata tests" [
+        test "Verify person temporary table metadata" {
+            let rows = [{ Id = 1L; Name = "" ; Patronymic = ""; Surname = "" }]
+            let table = { Name = "Persons"; Rows = rows }
+
+            let metadata = Metadata.Create table
+            let expectedColumns = [
+                { Name = "Id"        ; SqlType = "bigint"       ; AllowNull = false }
+                { Name = "Name"      ; SqlType = "nvarchar(max)"; AllowNull = false }
+                { Name = "Patronymic"; SqlType = "nvarchar(max)"; AllowNull = false }
+                { Name = "Surname"   ; SqlType = "nvarchar(max)"; AllowNull = false }
+            ]
+
+            Expect.equal metadata.Name "#Persons" "Wrong name of temporary table"
+            Expect.sequenceEqual metadata.Columns expectedColumns "Wrong columns metadata"
+        }
+    ]
