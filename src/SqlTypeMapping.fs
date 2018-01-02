5094f3ff13214f65877c74446d1cd868c7d87d57
alex troshkin <alextroshkin@outlook.com>
2018-01-02 20:09:14 +0300

    Fix typos in sql type mapping

--- src/SqlTypeMapping.fs
+++ src/SqlTypeMapping.fs
@@ -18,18 +18,18 @@ module SqlTypeMapping =
             { CLR = typeof<int>            ; SQL = DbType.Int32             ; SQL_Name = "int"}
             { CLR = typeof<uint32>         ; SQL = DbType.UInt32            ; SQL_Name = "bigint"}
             { CLR = typeof<int64>          ; SQL = DbType.Int64             ; SQL_Name = "bigint"}
-            { CLR = typeof<uint64>         ; SQL = DbType.UInt64            ; SQL_Name = "decimal(20}"}
+            { CLR = typeof<uint64>         ; SQL = DbType.UInt64            ; SQL_Name = "decimal(20)"}
             { CLR = typeof<float>          ; SQL = DbType.Single            ; SQL_Name = "real"}
             { CLR = typeof<double>         ; SQL = DbType.Double            ; SQL_Name = "float"}
-            { CLR = typeof<decimal>        ; SQL = DbType.Decimal           ; SQL_Name = "decimal(29,4}"}
+            { CLR = typeof<decimal>        ; SQL = DbType.Decimal           ; SQL_Name = "decimal(29,4)"}
             { CLR = typeof<bool>           ; SQL = DbType.Boolean           ; SQL_Name = "bit"}
-            { CLR = typeof<string>         ; SQL = DbType.String            ; SQL_Name = "nvarchar(max}"}
+            { CLR = typeof<string>         ; SQL = DbType.String            ; SQL_Name = "nvarchar(max)"}
             { CLR = typeof<char>           ; SQL = DbType.StringFixedLength ; SQL_Name = "nchar"}
             { CLR = typeof<Guid>           ; SQL = DbType.Guid              ; SQL_Name = "UNIQUEIDENTIFIER"}
             { CLR = typeof<DateTime>       ; SQL = DbType.DateTime          ; SQL_Name = "datetime2"}
             { CLR = typeof<DateTimeOffset> ; SQL = DbType.DateTimeOffset    ; SQL_Name = "DATETIMEOFFSET"}
             { CLR = typeof<TimeSpan>       ; SQL = DbType.Time              ; SQL_Name = "time"}
-            { CLR = typeof<byte array>     ; SQL = DbType.Binary            ; SQL_Name = "varbinary(max}"}
+            { CLR = typeof<byte array>     ; SQL = DbType.Binary            ; SQL_Name = "varbinary(max)"}
             { CLR = typeof<obj>            ; SQL = DbType.Object            ; SQL_Name = "sql_variant"}
         ]
 
