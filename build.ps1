13fd524213e0d198e2c8414df3d8a2983ffeefd8
alex troshkin <alextroshkin@outlook.com>
2018-01-15 03:32:14 +0300

    Change build script

--- build.ps1
+++ build.ps1
@@ -24,9 +24,6 @@ if (Test-Path .\artifacts) { Remove-Item .\artifacts -Force -Recurse }
 
 exec { & dotnet restore }
 
-$revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
-$revision = "{0:D4}" -f [convert]::ToInt32($revision, 10)
-
 exec { & dotnet run -p .\tests\FSharp.Data.Dapper.Tests -c Release }
 
-exec { & dotnet pack .\src\ -c Release -o .\artifacts\packages --version-suffix=$revision }
+exec { & dotnet pack .\src\ -c Release -o ..\artifacts\packages }
