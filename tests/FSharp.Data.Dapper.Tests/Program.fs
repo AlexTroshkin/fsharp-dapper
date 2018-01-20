078d23e86463bdd5164e526e58357439c3250d48
alex troshkin <alextroshkin@outlook.com>
2018-01-20 03:12:23 +0300

    Use option handler in tests

--- tests/FSharp.Data.Dapper.Tests/Program.fs
+++ tests/FSharp.Data.Dapper.Tests/Program.fs
@@ -1,9 +1,12 @@
 ﻿module Tests
 
 open Expecto
+open FSharp.Data.Dapper
 
 [<EntryPoint>]
 let main argv =
+    OptionHandler.RegisterTypes()
+
     InMemoryDatabase.Connection.Initialize()
     let ret = runTestsInAssembly defaultConfig argv
     InMemoryDatabase.Connection.Shutdown()
